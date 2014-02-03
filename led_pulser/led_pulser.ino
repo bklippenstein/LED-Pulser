6fdaf2607eb35934a3b60a096d72d64edbdcad64
Bryce Klippenstein <bklippenstein@carmanahsigns.com>
2014-02-03 15:51:42 -0700

    Release 1.0
    First release.
    will probably need to adjust the timing but we'll see what it looks like
    in the sign.

--- led_pulser/led_pulser.ino
+++ led_pulser/led_pulser.ino
@@ -1,21 +1,51 @@
 
+#include <SoftwareSerial.h>
 
 int i=0; //contains loop index
-double brightness; //contains output of triganometric brightness function
-double angle; //contains the angle in radins of the triganometric brightness function
-int pwm; //contains the actual brightness of the LED
+double brightness = 0; //contains output of triganometric brightness function
+double angle = 0; //contains the angle in radins of the triganometric brightness function
+int pwm = 0; //contains the actual brightness of the LED
+int max_index = 256;
+int serialDebug = 0;
 
 void setup() {
-  
+  // initialize serial communications at 9600 bps:
+  if(serialDebug)
+  {
+    Serial.begin(9600); 
+    Serial.print("Begin.\n");
+  }
 }
 
 void loop() {
   
   i++;
-  if(i=256)
+  if(i==256)
   {
     i=0;
   }
-  analogWrite(3,i); 
-  delay(100);
+  angle = 6.283*i/255;
+   if(serialDebug)
+  {
+    Serial.print("angle: ");
+    Serial.print(angle,5);
+  }
+
+  brightness = 1 + sin(angle);
+  if(serialDebug)
+  {
+    Serial.print(" brightness: ");
+    Serial.print(brightness,5);
+  }
+
+  pwm = brightness * 128;
+  if(serialDebug)
+  {
+    Serial.print(" pwm: ");
+    Serial.print(pwm);
+    Serial.print("\n");
+  }
+  
+  analogWrite(3,pwm); 
+  delay(10);
 }
